f7164eb4674ad4f3323665d1848f075b74273492
Talon Revis <beacon.web3@gmail.com>
2024-02-10 20:41:59 -0600

    first commit with all changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom'
 import axios from 'axios';
 
+import Movie from './Movies/Movie';
+import MovieList from './Movies/MovieList';
 import SavedList from './Movies/SavedList';
 
 export default function App () {
@@ -11,7 +14,10 @@ export default function App () {
     const getMovies = () => {
       axios
         .get('http://localhost:5001/api/movies') // Study this endpoint with Postman
-        .then(response => {
+        .then(response => { 
+          
+          setMovies(response.data);
+
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movies' slice of state
         })
@@ -30,7 +36,11 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      {/* <div>Replace this Div with your Routes</div> */}
+      <Routes> 
+        <Route path='/' element={<MovieList movies={movies} />} />
+        <Route path='movies/:id' element={<Movie />} />
+      </Routes>
     </div>
   );
 }
